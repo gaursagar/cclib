f8a13a49b3daf6b82df51f1c0512ffc52c465b31
Sagar Gaur <gaursagar21@gmail.com>
2017-06-23 21:03:01 +0530

    Adding molden test and reference files.

--- test/io/testmoldenwriter.py
+++ test/io/testmoldenwriter.py
@@ -12,6 +12,9 @@ import unittest
 
 import cclib
 from cclib.io.filewriter import MissingAttributeError
+from cclib.parser.utils import MoldenReformatter
+
+from difflib import ndiff
 
 __filedir__ = os.path.dirname(__file__)
 __filepath__ = os.path.realpath(__filedir__)
@@ -31,6 +34,20 @@ class MOLDENTest(unittest.TestCase):
         with self.assertRaises(MissingAttributeError):
             cclib.io.moldenwriter.MOLDEN(data)
 
+    def test_molden_cclib_diff(self):
+        """Check if file written by cclib matched file written by Molden."""
+        fpath = os.path.join(__datadir__,
+                             "data/GAMESS/basicGAMESS-US2014/C_bigbasis.out")
+        data = cclib.io.ccopen(fpath).parse()
+        cclib_out = cclib.io.moldenwriter.MOLDEN(data).generate_repr()
+        cclib_out = MoldenReformatter(cclib_out).reformat()
+        fpath = os.path.join(__datadir__,
+                             "data/GAMESS/basicGAMESS-US2014/molden_ref/C_bigbasis.molden")
+        molden_out = open(fpath).read()
+        molden_out = MoldenReformatter(molden_out).reformat()
+        # Molden files cannot be wriiten if atomcoords are missing.
+        for i in ndiff(cclib_out, molden_out):
+            print(i)
 
 if __name__ == "__main__":
     unittest.main()
